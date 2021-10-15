13709c6bd9092f18d6f644433de5eb50ffcaecdb
knmcdaniel <mcbreezy1994@gmail.com>
2021-10-14 23:00:20 -0500

    login and reg

---
+++ 03-java-spring/05-login-and-registration/01-controller-and-views/src/main/java/com/keith/controllerandviews/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.keith.controllerandviews.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.keith.controllerandviews.models.User;
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
+}
---
+++ 03-java-spring/05-login-and-registration/01-controller-and-views/src/test/java/com/keith/controllerandviews/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keith.controllerandviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
